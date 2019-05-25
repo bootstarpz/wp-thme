7518798ead1a5f4e7ec5b40b6bfb6634b918473f
bootstarpz <50875669+bootstarpz@users.noreply.github.com>
2019-05-25 12:42:33 +0600

    Update zxtn.js

--- zxtn.js
+++ zxtn.js
@@ -1,1 +1,644 @@
 //zxtn
+<!DOCTYPE html>
+
+ <html>
+	<title>Sensei was here! </title>
+<head>
+
+<meta name="Author" content="Sensei of Crimson Hacker(s)"/>
+    <meta name="copyright" content="Sensei of Crimson Hacker(s)"/>
+    <meta name="keywords" content="Sensei of Crimson Hacker(s), Hacked , Hacked, Hacked , Hacked , Hacked, Hacked , Hacked , Hacked , Hacked , Hacked , Hacked"/>
+    <meta name="description" content="Hell is Empty,All Devils are Here."/>
+    <meta property="og:title" content="Hacked by Sensei | Crimson Hacker(s)"/>
+	<meta property="og:image" content="-9/14040141_198339717248746_5685180715013902098_n.jpg?oh=ce8a299a3d38e39f33b07b02192f0b73&oe=583DF1BD"/> 
+	<script type="text/javascript">
+		function hey() {
+			document.getElementById("card_z").style.display="none";
+			document.getElementById("card_a").style.display="block";
+		}
+
+
+
+
+
+<link rel="icon" href="file:https://s9.postimg.org/q5tc09asv/CHSLogo.png"/>
+
+<title>Hacked by:Sensei  </title>
+<script>Crimson Hacker(s) was Here!</script>
+<embed src="https://www.youtube.com/embed/IzZMG1xWvk4&autoplay=1" type="application/x-shockwave-flash" wmode="transparent" width="1" height="1"></embed>
+
+ 
+
+ 
+<iframe width="1" height="1" frameborder="0" allowfullscreen></iframe>
+ 
+<link href='https://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'>
+ 
+
+ 
+<body oncontextmenu='return false;' onclick='return false;' onkeydown='return false;' onmousedown='return false;'>
+<style type="text/css">
+ 
+body { 
+ 
+ background: black url("http://img01.deviantart.net/f36a/i/2011/247/5/c/red_galaxy_wallpaper_by_xtreemgamer129-d48t5lb.png");
+background-repeat: repeat;
+background-position: center;
+background-attachment: fixed;
+background-size: cover;
+}
+  
+      	background-repeat: no-repeat;
+	-webkit-animation-name: blinker; 
+	-webkit-animation-duration:1s ;
+    animation-name: blinker;
+    animation-duration:1;
+    animation-iteration-count: 1000;
+}
+@-webkit-keyframes change {
+	0%{background-color:black;}
+    100%{background-color:black;}
+}
+@keyframes change {
+	0%{background-color:black;}
+    100%{background-color:black;}
+}
+h1{
+	font-family: 'Orbitron' , sans-serif;
+	text-shadow:black 1px 1px 3px;
+}
+h3{
+	font-family: 'Orbitron' , sans-serif;
+	text-shadow:black 1px 1px 3px;
+}
+p{
+	font-family: 'Orbitron' , sans-serif;
+	text-shadow:black 1px 1px 3px;
+}
+audio{
+	visibility:hidden;
+}
+#we{
+	margin-left:150px;
+	margin-right:150px;
+}
+ 
+ 
+</style> 
+ 
+<br>
+ 
+<center><img src="https://s9.postimg.org/q5tc09asv/CHSLogo.png" width="300px" height="300px"></center>
+
+ 
+ 
+ 
+ 
+<body onload=writetext() onLoad="setTimeout('delayer()', 2000)">
+ 
+ 
+
+ <p align=center></p> 
+ 
+
+ 
+<SCRIPT language=JavaScript> 
+ 
+ 
+msg = new Array(); //strings written in screen
+ 
+ 
+
+
+msg[0] = "<h2><font face='Orbitron' color='red' new size='6'><center>Hacked by:Sensei</center></font></h2>";
+ 
+ 
+ 
+ 
+   
+ 
+ 
+msg[1] = "<font face='Orbitron' color='red' new size='5'><center>Sensei of Crimson Hacker(s)<br></center></font></b>"; 
+ 
+ 
+ 
+ 
+ 
+   
+ 
+
+ 
+
+ 
+msg[2] = "<font face='Orbitron' color='red' new size='3'><center><br>We are:<font face='Orbitron' color='red'><br> Kush | SyNt4xERROR | <u>Sensei</u> | S3R10US | PH-N | Num3r0 | m4R-1 | 4$us | 4sh3r | X0110010010 | M3g4 b4t | z14Yp!� | z4pp4 | L0V3 | 570rm 5p1r17 |  </font></center><br></font>"; 
+msg[3] = "<font face='Orbitron' color='red' new size='3'><center><br>Greetings to:<font face='Orbitron' color='red'><br> BloodSec International | Anonymous Philippines | Philippine Cyber Hacking | GlobalSec Hackers | DeadSecurity Cyber Army | Silangan Cyber Army | Anonywolves PH | Magdalo Cyber Army | Rizal Vendetta | and to all Pilipino Hackers </font></center><br></font>"; 
+ 
+ 
+ 
+text1 = ""; //the same as text2, only the last character is highlighted
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+text2 = ""; //current string, which will be written
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+count = 0; //char index in string text
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+count2 = 0; //number of strings
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+text = msg[0].split(""); //text - string written
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+function writetext() { //show strings above on screen
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+text1 = text2 + "<font color='#000000'>" + text[count] + "</font>";
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+text2 += text[count];
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+document.all["nothing"].innerHTML = text1; //where to write
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+if (count < text.length-1){
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+count++;
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+setTimeout('writetext()', 34);
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+else { //if this string is written, get the new string
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+count = 0;
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+if (count2 != 14) { //write 14 strings
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+count2++;
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+text2 += ""; //a new line
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+text = eval('msg['+count2+'].split("")'); //get the new string to text
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+setTimeout('writetext()', 1);
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+</SCRIPT> 
+ 
+ 
+ 
+ 
+   
+  
+ 
+ 
+ 
+<SCRIPT LANGUAGE="JavaScript"> 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+<!-- Disable
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+function disableselect(e){
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+return false
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+function reEnable(){
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+return true
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+//if IE4+
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+document.onselectstart=new Function ("return false")
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+document.oncontextmenu=new Function ("return false")
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+//if NS6
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+if (window.sidebar){
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+document.onmousedown=disableselect
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+document.onclick=reEnable
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+//-->
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+</script> 
+ 
+ 
+
+
+   
+ 
+   
+ 
+ 
+ 
+<DIV id=nothing style="FONT-FAMILY:Orbitron'"> 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+<H2><U> 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+   
+ 
+ 
+ 
+</B></FONT></P> 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+<P>Hacking..</P>%2
+
+<!-- www.000webhost.com Analytics Code -->
+
+<script type="text/javascript" src="http://analytics.hosting24.com/count.php"></script>
+
+<noscript><a href="http://www.hosting24.com/"><img src="http://analytics.hosting24.com/count.php" alt="web hosting" /></a></noscript>
+
+<!-- End Of Analytics Code -->
+
+
+</head>
+
+</html>
